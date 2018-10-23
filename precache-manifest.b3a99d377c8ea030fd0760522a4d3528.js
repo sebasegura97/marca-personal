9e3051b55bfb0890f30f017ae549c703953b9262
sebasegura97 <sebasegura97@gmail.com>
2018-10-22 22:00:23 -0300

    Updates

---
+++ precache-manifest.b3a99d377c8ea030fd0760522a4d3528.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc9e427970b2a9702f8fa1d3e5bfa17e",
+    "url": "/marca-personal/static/media/wpp.bc9e4279.svg"
+  },
+  {
+    "revision": "0913b1d19b4fb0ae5233",
+    "url": "/marca-personal/static/css/main.32d6a4f6.chunk.css"
+  },
+  {
+    "revision": "16fb6185b61cc076140fef79b9ba3b4a",
+    "url": "/marca-personal/static/media/background.16fb6185.jpg"
+  },
+  {
+    "revision": "12c8e166990c79ea6314",
+    "url": "/marca-personal/static/js/1.12c8e166.chunk.js"
+  },
+  {
+    "revision": "d078a40dfde1db087a16",
+    "url": "/marca-personal/static/js/runtime~main.d078a40d.js"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/marca-personal/static/media/react.7c0fdc49.png"
+  },
+  {
+    "revision": "f79a9399e188fce8f0c56645eeafe338",
+    "url": "/marca-personal/static/media/redux.f79a9399.jpg"
+  },
+  {
+    "revision": "998e01d20b335e7b9565490c33878894",
+    "url": "/marca-personal/static/media/image1.998e01d2.png"
+  },
+  {
+    "revision": "71a405159a6a1b264661ccb47a54b675",
+    "url": "/marca-personal/static/media/js.71a40515.png"
+  },
+  {
+    "revision": "136cfd5db1a02f2cffee5d6307a7e5fc",
+    "url": "/marca-personal/static/media/github.136cfd5d.svg"
+  },
+  {
+    "revision": "0b7574878088eafd9c649e041606403b",
+    "url": "/marca-personal/static/media/dayspa.0b757487.svg"
+  },
+  {
+    "revision": "f79fd6051949fe394ab5a087fc311004",
+    "url": "/marca-personal/static/media/tasafin.f79fd605.svg"
+  },
+  {
+    "revision": "bee22f8a31f156dad4a07f4bc850662a",
+    "url": "/marca-personal/static/media/cayro.bee22f8a.svg"
+  },
+  {
+    "revision": "b3d419028a40b4bce1afe16e5dcadb9b",
+    "url": "/marca-personal/static/media/snap.b3d41902.svg"
+  },
+  {
+    "revision": "f81dd93b282b4a3551cf6396b982f77e",
+    "url": "/marca-personal/static/media/phone.f81dd93b.svg"
+  },
+  {
+    "revision": "7dcdb7c5bfb0cace12b6562fd8cee649",
+    "url": "/marca-personal/static/media/instagram.7dcdb7c5.svg"
+  },
+  {
+    "revision": "0913b1d19b4fb0ae5233",
+    "url": "/marca-personal/static/js/main.0913b1d1.chunk.js"
+  },
+  {
+    "revision": "2c6ead26164b357f47e2e9a8665e02c5",
+    "url": "/marca-personal/static/media/image2.2c6ead26.png"
+  },
+  {
+    "revision": "fde7339e96b22f8872d586a2663c3bd0",
+    "url": "/marca-personal/static/media/diploma-bots-fb.fde7339e.png"
+  },
+  {
+    "revision": "9e106eb0682613c49866b41c6aa74eba",
+    "url": "/marca-personal/static/media/diploma-desarrollo-react.9e106eb0.png"
+  },
+  {
+    "revision": "3d15e3c4bdf06b72e982c92aaf2fdd5a",
+    "url": "/marca-personal/static/media/diploma-diseno-interfaces-ux.3d15e3c4.png"
+  },
+  {
+    "revision": "fb8485d096cf41475fa85e14481f3450",
+    "url": "/marca-personal/static/media/diploma-graphql.fb8485d0.png"
+  },
+  {
+    "revision": "f4938424a9a0808611e0b5d1db27a363",
+    "url": "/marca-personal/static/media/diploma-html5-css3.f4938424.png"
+  },
+  {
+    "revision": "8058b6379df7ba2571ae5e0f44cf08e5",
+    "url": "/marca-personal/static/media/diploma-illustrator.8058b637.png"
+  },
+  {
+    "revision": "0ab26699df2eb4977919acedc74c7049",
+    "url": "/marca-personal/static/media/diploma-react-native.0ab26699.png"
+  },
+  {
+    "revision": "2904f6a097c7f58a236948b1ce171021",
+    "url": "/marca-personal/static/media/diploma-react.2904f6a0.png"
+  },
+  {
+    "revision": "1d0afb60bb8e404a0ff91326517d4a8f",
+    "url": "/marca-personal/static/media/diploma-redux.1d0afb60.png"
+  },
+  {
+    "revision": "1ae70e8aa0f1b0d040eb9e7242db6109",
+    "url": "/marca-personal/static/media/diploma-responsive-design.1ae70e8a.png"
+  },
+  {
+    "revision": "27e50ffd6a14cbc8221c9dbd3b5208dc",
+    "url": "/marca-personal/static/media/Montserrat-Regular.27e50ffd.otf"
+  },
+  {
+    "revision": "6d1796a9f798ced8961baf3c79f894b6",
+    "url": "/marca-personal/static/media/Montserrat-Black.6d1796a9.ttf"
+  },
+  {
+    "revision": "e58c8fb2a8e31b9997c16b8a3839b62a",
+    "url": "/marca-personal/static/media/Back.e58c8fb2.svg"
+  },
+  {
+    "revision": "12c8e166990c79ea6314",
+    "url": "/marca-personal/static/css/1.26eed22c.chunk.css"
+  },
+  {
+    "revision": "257cd14b78887e52ede536417d41e23f",
+    "url": "/marca-personal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/marca-personal/precache-manifest.9f3cdd00813c13cfc057955bd33b778b.js"
+  "/marca-personal/precache-manifest.b3a99d377c8ea030fd0760522a4d3528.js"
 );
 
 workbox.clientsClaim();
